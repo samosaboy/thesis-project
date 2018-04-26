e7d7ebf4b407a5233474fee6326b31a79de0f89f
Nikunj Varshney <varshney.nikunj@gmail.com>
2018-04-25 21:39:57 -0400

    Minor adjustments to stats

--- src/app/components/Root.tsx
+++ src/app/components/Root.tsx
@@ -90,11 +90,11 @@ export class Root {
     /*
      * Instantiate Stats for Development
      * */
+    this.stats = new Stats()
+    this.stats.showPanel(0)
+    this.stats.dom.style.right = 0
+    this.stats.dom.style.left = 'auto'
     if (isDev) {
-      this.stats = new Stats()
-      this.stats.showPanel(0)
-      this.stats.dom.style.right = 0
-      this.stats.dom.style.left = 'auto'
       document.body.appendChild(this.stats.dom)
     }
 
